33179cf7f815a0541137bbc86835cf995541fbf1
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-04-04 20:47:34 +1000

    Update check_bucket_size.py

--- check_bucket_size.py
+++ check_bucket_size.py
@@ -1,60 +1,77 @@
 import boto3
 import datetime
+import sys
 
-regions = ['ap-southeast-2']
+account_id = boto3.client('sts').get_caller_identity().get('Account')
+orig_stdout = sys.stdout
+f = open('Bucket_size_larger_than_1TB_%s.txt' % account_id, 'w')
+sys.stdout = f
+
+
+ec2 = boto3.client('ec2')
+regions = [region['RegionName']
+               for region in ec2.describe_regions()['Regions']]
+
+regions=["ap-southeast-2"]
 measurable_metrics = [
     ('BucketSizeBytes', 'StandardStorage'),
     ('NumberOfObjects', 'AllStorageTypes'),
 ]
 
-s3 = boto3.resource('s3')
+for region in regions:
+    print(region)
+    s3 = boto3.resource('s3',region)
 
-def cw(region, cws = {}):
-    '''Caching Cloudwatch accessor by region.'''
+    def cw(region, cws = {}):
+        '''Caching Cloudwatch accessor by region.'''
 
-    if region not in cws:
-        cws[region] = boto3.client('cloudwatch', region_name = region)
+        if region not in cws:
+            cws[region] = boto3.client('cloudwatch', region_name = region)
 
-    return cws[region]
+        return cws[region]
 
-def bucket_stats(name, date):
-    '''Get the number object count and size of a bucket on a given date.'''
+    def bucket_stats(name, date):
+        '''Get the number object count and size of a bucket on a given date.'''
 
-    results = {}
+        results = {}
 
-    for metric_name, storage_type in measurable_metrics:
-        for region in regions:
-            metrics = cw(region).get_metric_statistics(
-                Namespace = 'AWS/S3',
-                MetricName = metric_name,
-                StartTime = date - datetime.timedelta(days = 365),
-                EndTime = date,
-                Period = 86400,
-                Statistics = ['Average'],
-                Dimensions = [
-                    {'Name': 'BucketName', 'Value': name},
-                    {'Name': 'StorageType', 'Value': storage_type},
-                ],
-            )
+        for metric_name, storage_type in measurable_metrics:
+            #for region in regions:
+                metrics = cw(region).get_metric_statistics(
+                    Namespace = 'AWS/S3',
+                    MetricName = metric_name,
+                    StartTime = date - datetime.timedelta(days = 365),
+                    EndTime = date,
+                    Period = 86400,
+                    Statistics = ['Average'],
+                    Dimensions = [
+                        {'Name': 'BucketName', 'Value': name},
+                        {'Name': 'StorageType', 'Value': storage_type},
+                    ],
+                )
 
-            if metrics['Datapoints']:
-                results[metric_name] = sorted(metrics['Datapoints'], key = lambda row: row['Timestamp'])[-1]['Average']
-                results['region'] = region
-                continue
+                if metrics['Datapoints']:
+                    results[metric_name] = sorted(metrics['Datapoints'], key = lambda row: row['Timestamp'])[-1]['Average']
+                    results['region'] = region
+                    continue
 
-    return results
+        return results
 
-date = datetime.datetime.utcnow().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+    date = datetime.datetime.utcnow().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
 
-print('name', 'region', 'TB size', 'objects', sep = '\t')
-res = []
+    print('name', 'region', 'TB size', 'objects', sep = '\t')
+    res = []
 
-for bucket in sorted(s3.buckets.all(), key = lambda bucket: bucket.name):
-    results = bucket_stats(bucket.name, date)
-    if int(results.get('BucketSizeBytes', 0)) >= 1099511627776:
-        res.append((bucket.name, results.get('region'), int(results.get('BucketSizeBytes', 0))/(1024*1024*1024*1024), int(results.get('NumberOfObjects', 0))))
-        #print(bucket.name, results.get('region'), int(results.get('BucketSizeBytes', 0))/(1024*1024*1024*1024), int(results.get('NumberOfObjects', 0)), sep = '\t')
+    for bucket in sorted(s3.buckets.all(), key = lambda bucket: bucket.name):
+        results = bucket_stats(bucket.name, date)
+        if int(results.get('BucketSizeBytes', 0)) >= 1099511627776:
+            res.append((bucket.name, results.get('region'), int(results.get('BucketSizeBytes', 0))/(1024*1024*1024*1024), int(results.get('NumberOfObjects', 0))))
+            #print(bucket.name, results.get('region'), int(results.get('BucketSizeBytes', 0))/(1024*1024*1024*1024), int(results.get('NumberOfObjects', 0)), sep = '\t')
 
-res.sort(key=lambda x: x[2], reverse=True)
-for item in res:
-    print(item)
+    res.sort(key=lambda x: x[2], reverse=True)
+    for item in res:
+        print(item)
+
+
+sys.stdout = orig_stdout
+f.close()
